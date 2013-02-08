921e7f90a335e6cecee45cd7d36ad0c3f6dede4a
Nick Barnwell <nb@ul.io>
2013-02-08 12:02:10 -0800

    Fns for interacting with the tumblr API
    Corgi-specific fns also included

--- src/cuture/tumblr.clj
+++ src/cuture/tumblr.clj
@@ -13,11 +13,34 @@
 
 (defn tumblr-request
   ([fnk positionals] (tumblr-request fnk positionals nil))
-  ([fnk positionals data] (tumblr-request positionals data nil))
+  ([fnk positionals data] (tumblr-request fnk positionals data nil))
   ([fnk positionals data opts]
-  (let [url (create-url positionals)
-        data (if opts (merge data opts) data)
-        final-opts (merge-with conj (assoc-in data [:query-params "api_key"] api-key))]
-     (fnk url final-opts))))
+    (let [url (create-url positionals)
+          request-opts (merge {:as :json} opts)
+          all-options (merge data request-opts)
+          payload (assoc-in all-options [:query-params "api_key"] api-key)]
+       (fnk url payload))))
 
+(defn extract-posts [resp]
+  "Extract Posts collection from Tumblr API response."
+  (get-in resp [:body :response]))
 
+(defn get-posts-tagged [tags]
+  "Retrieve posts tagged with [tags] from tumblr API"
+  (extract-posts (tumblr-request client/get [:tagged] 
+    {:query-params {"tag" (clojure.string/join (map name tags))}})))
+
+(defn largest-photos-in-post [post]
+  (let [photos (:photos post)]
+    (for [itm photos
+          :let [sizes (:alt_sizes itm)]]
+      (apply max-key :width sizes)))) 
+
+(defn fetch-corgis 
+  ([] (first (fetch-corgis 1)))
+  ([n]
+  "Fetches n-more corgis from tumblr"
+  (let [corgis! (into [] 
+                  (flatten (map 
+                     largest-photos-in-post (get-posts-tagged ["corgi"]))))]
+  (map :url (take n (repeatedly #(rand-nth corgis!)))))))
