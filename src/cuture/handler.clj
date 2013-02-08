d6fe06d74b1f7c68ea347e48185eb22e64008b5a
Nick Barnwell <nb@ul.io>
2013-02-08 11:58:29 -0800

    Handler now parses integers and calls proper fetch fn

--- src/cuture/handler.clj
+++ src/cuture/handler.clj
@@ -1,14 +1,14 @@
 (ns cuture.handler
   (:use compojure.core)
-  (:use [cuture.tumblr :only (get-corgis)])
+  (:use [cuture.tumblr :only (fetch-corgis)])
   (:require [compojure.handler :as handler]
             [compojure.route :as route]))
 
 (defroutes app-routes
   (GET "/" [] "Here be Corgis. Try hitting /random")
-  (GET "/random" [] (get-corgis))
+  (GET "/random" [] (fetch-corgis))
   (GET ["/random/:num" :num #"[0-9]"] [num]
-       (get-corgis num))
+       (clojure.string/join "," (fetch-corgis (Integer/parseInt num))))
   (route/not-found "Not Found"))
 
 (def app
