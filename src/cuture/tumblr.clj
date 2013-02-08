c141e3ec9cdeb36b7053f2e625f394477fed7143
Nick Barnwell <nb@ul.io>
2013-02-08 08:03:50 -0800

    Tumblr API base funcs

--- src/cuture/tumblr.clj
+++ src/cuture/tumblr.clj
@@ -6,3 +6,18 @@
   ([n] (repeat n nil)))
 
 
+(defn create-url [positionals]
+  (let [base-url "http://api.tumblr.com/v2"
+        components (cons base-url positionals)]
+    (clojure.string/join "/" (map name components))))
+
+(defn tumblr-request
+  ([fnk positionals] (tumblr-request fnk positionals nil))
+  ([fnk positionals data] (tumblr-request positionals data nil))
+  ([fnk positionals data opts]
+  (let [url (create-url positionals)
+        data (if opts (merge data opts) data)
+        final-opts (merge-with conj (assoc-in data [:query-params "api_key"] api-key))]
+     (fnk url final-opts))))
+
+
