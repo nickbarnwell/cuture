791f78f1ccd0efbecfa90bd3b522aca554ed4d4e
Nick Barnwell <nb@ul.io>
2013-03-01 17:33:01 -0800

    Add shell script sample for SMS

---
+++ scripts/send-sms.sh
@@ -0,0 +1,13 @@
+# Description
+#   Sends a link to a Corgi photo from Tumblr to a given number
+#
+# Variables:
+#   GV_EMAIL=Google Voice Username
+#   GV_PW=Google Voice Password
+#   NUM=Comma delimited list of numbers to send to
+
+#Requires pygvoice to be in your $PATH
+#Add to crontab like so, or use Immutant+Twilio (TODO:Nick)
+# > 0 */3 * * * $HOME/corgi-sms.sh >> $HOME/corgi-log.txt
+gvoice -e $GV_EMAIL --password=$GV_PW --batch send_sms $num `curl -s http://cuture.herokuapp.com/random`
+echo "Sent something at `date '+%H:%M:%S - %m/%d/%y'`"
